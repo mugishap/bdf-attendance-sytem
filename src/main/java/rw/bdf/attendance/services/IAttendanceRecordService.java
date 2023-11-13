5af38ff89d550dd4e996b4271531339f95be548b
mugishap <precieuxmugisha@gmail.com>
2023-11-14 00:01:01 +0200

    [feat]: Adding attendance record service

--- src/main/java/rw/bdf/attendance/services/IAttendanceRecordService.java
+++ src/main/java/rw/bdf/attendance/services/IAttendanceRecordService.java
@@ -2,8 +2,20 @@ package rw.bdf.attendance.services;
 
 import rw.bdf.attendance.models.AttendanceRecord;
 
+import java.util.UUID;
+
 public interface IAttendanceRecordService {
 
     AttendanceRecord create();
+
+    AttendanceRecord update();
+
+    AttendanceRecord findById();
+
+    AttendanceRecord findAll();
+
+    AttendanceRecord findByUserId(UUID userId);
+
+    void delete();
 
 }
